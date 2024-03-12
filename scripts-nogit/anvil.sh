187d8e62222e8a9166b272b4c9d98ef3eaa84375
vg40506 <gazula@gmail.com>
2024-03-12 00:40:23 -0400

    Delete scripts-nogit directory

--- scripts-nogit/anvil.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/gazula/ACCESS/Software/Anvil
-ssh x-gazula@anvil.rcac.purdue.edu " cd /home/x-gazula/software; sh /home/x-gazula/software/extract.sh"
-scp x-gazula@anvil.rcac.purdue.edu:/home/x-gazula/software/anvil* .
-git add .
-git push -u origin main
