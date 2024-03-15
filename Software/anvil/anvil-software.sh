db22320409b9b450392bdf14cbae541e30f5cae1
VG <gazula@gmail.com>
2024-03-15 19:10:23 -0400

    Updating Software list

--- Software/anvil/anvil-software.sh
+++
@@ -1,4 +0,0 @@
-cd /home/x-gazula/software
-sh extract.sh
-scp -o IdentitiesOnly=yes -i ~x-gazula/.ssh/id_rsa *.txt beacon@access-ara.ccs.uky.edu:software/anvil
-scp -o IdentitiesOnly=yes -i ~x-gazula/.ssh/id_rsa extract.sh beacon@access-ara.ccs.uky.edu:software/anvil
--- Software/kyric/kyric-software.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/gazula/software
-sh extract.sh
-scp -o IdentitiesOnly=yes -i ~gazula/.ssh/id_rsa *.txt beacon@access-ara.ccs.uky.edu:software/kyric
-scp -o IdentitiesOnly=yes -i ~gazula/.ssh/id_rsa extract.sh beacon@access-ara.ccs.uky.edu:software/kyric
