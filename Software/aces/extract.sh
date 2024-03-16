18bb47fc4f33b8f63d2f310dbb49218ddbb126c0
VG <gazula@gmail.com>
2024-03-16 16:14:07 -0400

    Updating Software list

--- Software/aces/extract.sh
+++ Software/aces/extract.sh
@@ -6,4 +6,5 @@ rm -rf *.txt
 module --redirect spider > module_spider_output_cpu.txt
 
 # Use AWK to filter lines where the first word ends with a colon and extract the first word without the colon
-awk -F'[:/]' '!/^ *$/ && $0 ~ /:/ {gsub(/^ +/, "", $1); gsub(/ +$/, "", $1); if (!seen[$1]++ && $1 !~ / /) print tolower($1)}' module_spider_output_cpu.txt > aces-cpu-software.txt
+awk -F'[:/]' '!/^ *$/ && $0 ~ /:/ {gsub(/^ +/, "", $1); gsub(/ +$/, "", $1); if (!seen[$1]++ && $1 !~ / /) print tolower($1)}' module_spider_output_cpu.txt | sort -u > aces-cpu-software.txt
+
