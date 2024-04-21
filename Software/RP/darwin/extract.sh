afdfc4072725cc1f31b1801cb935161bbd11e2f3
vg40506 <gazula@gmail.com>
2024-04-20 23:45:51 -0400

    Delete Software/RP directory

--- Software/RP/darwin/extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf *.txt
-
-# Run module spider and redirect output to a file
-vpkg_list > vpkg_list_output_cpu.txt
-cp vpkg_list_output_cpu.txt darwin-cpu-software.txt
-sed -i '1,3d'  darwin-cpu-software.txt
-scp * beacon@access-ara.ccs.uky.edu:/home/beacon/software/darwin
